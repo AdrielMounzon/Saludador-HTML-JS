8dccccfcbb9681f3765b0f2b39d6dd6e57ee8941
Adriel <adriel.mounzon@ucb.edu.bo>
2023-02-19 11:30:30 -0400

    Saludar por nombre del usuario

--- src/saludador.js
+++ src/saludador.js
@@ -1,16 +1,9 @@
 const nombre = document.querySelector("#nombre");
 const form = document.querySelector("#saludador-form");
 const div = document.querySelector("#resultado-div");
-const genero = document.querySelector("#genero");
 
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-
-    let gen;
-    if(genero.value=="hombre")
-        gen = "Sr.";
-    if (genero.value=="mujer")
-        gen = "Sra. ";
-  div.innerHTML = "<p> hola " + gen + nombre.value + "</p>";
+  div.innerHTML = "<p> hola "  + nombre.value + "</p>";
 });
